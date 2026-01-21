176165c7126733a20eef45a0d5df641d1bbe1a02
SteveZhang <871506263@qq.com>
2026-01-21 23:24:21 +0800

    1

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
-# 全局作用域
-global_var = "我是全局变量"
+# Python 全局变量修改
+counter = 0
 
 
-def test_scope():
-    # 函数作用域
-    function_var = "我是函数内变量"
-
-    if True:
-        # Python 没有块级作用域！
-        block_var = "我是块级变量"
-        print("块内访问:", block_var)
-        print("块内访问函数变量:", function_var)
-        print("块内访问全局变量:", global_var)
-
-    # 块外仍然可以访问块级变量
-    print("函数内访问块级变量:", block_var)
-    print("函数内访问函数变量:", function_var)
-    print("函数内访问全局变量:", global_var)
+def increment():
+    global counter  # 声明使用全局变量
+    counter += 1
+    print("计数器:", counter)
 
 
-test_scope()
-print("全局访问全局变量:", global_var)
-# print(function_var)  # 错误！
+def increment_local():
+    counter = 0  # 创建局部变量
+    counter += 1
+    print("局部计数器:", counter)
+
+
+increment()
+increment()
+increment_local()
+print("全局计数器:", counter)
