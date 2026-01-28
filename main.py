534eff30875c09e278249c7e2109904d2f0c8455
SteveZhang <871506263@qq.com>
2026-01-28 16:10:57 +0800

    1

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from math_utils import add, subtract, Calculator
 
-calc = Calculator()
-
 print(add(5, 3))  # 8
 print(subtract(10, 3))  # 7
 
 print("----------------")
+
+calc = Calculator()
 
 print(calc.add(10).add(4).result)  # 14
 print(calc.subtract(10).subtract(2).result)  # 6
