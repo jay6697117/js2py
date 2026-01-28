0e9ccc34fb93ef6da549ae5797f5115b4517d78e
SteveZhang <871506263@qq.com>
2026-01-28 16:12:10 +0800

    1

---
+++ demos/模块系统/main.py
@@ -0,0 +1,13 @@
+from math_utils import add, subtract, Calculator
+
+print(add(5, 3))  # 8
+print(subtract(10, 3))  # 7
+
+print("----------------")
+
+calc = Calculator()
+
+print(calc.add(10).add(4).result)  # 14
+print(calc.subtract(10).subtract(2).result)  # 6
+
+print(calc.result)  # 2
